bbff9bed6a261ae55c0c60f4bb9a690d0c4b87ce
hunterhector <hunterhector@gmail.com>
2014-09-03 15:14:45 -0400

    Initial commit

---
+++ src/main/java/edu/cmu/cs/lti/cds/FullSystemRunner.java
@@ -0,0 +1,8 @@
+package edu.cmu.cs.lti.cds;
+
+public class FullSystemRunner {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/main/java/edu/cmu/cs/lti/cds/TemporalGrouper.java
@@ -0,0 +1,11 @@
+package edu.cmu.cs.lti.cds;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: zhengzhongliu
+ * Date: 9/1/14
+ * Time: 7:43 PM
+ */
+public class TemporalGrouper {
+
+}
