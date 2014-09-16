13e854b36d75ae996ec43b60a1b2448c76e793a0
Hector <hunterhector@gmail.com>
2014-09-16 01:36:51 -0400

    add simple running shell

---
+++ run.sh
@@ -0,0 +1,1 @@
+mvn exec:java -Dexec.mainClass="edu.cmu.cs.lti.cds.PreparationPipeline" -Dexec.args="/home/hector/projects/gigascript_exp/data/giga_ny/ /home/hector/projects/uima-base-tools/fanse-parser/src/main/resources/"
