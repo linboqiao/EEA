d9d7bdb7c99ffdc3cbb5f2ea105393acdbae9157
Hector <hunterhector@gmail.com>
2014-10-15 13:30:36 -0400

    remove nohpu

--- run_cairo.sh
+++
@@ -1,2 +0,0 @@
-export MAVEN_OPTS="-Xmx4g" 
-mvn exec:java -Dexec.mainClass="edu.cmu.cs.lti.cds.runners.PreparationPipeline" -Dexec.args='/home/hector/projects/gigascript_exp/data/giga_ny/'$1' agiga/'$1' /home/hector/projects/uima-base-tools/fanse-parser/src/main/resources/'
