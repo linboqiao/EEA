ed74036dd1ef486fa7287d99e9973bce3cb4f7ed
Hector <hunterhector@gmail.com>
2014-10-27 19:39:21 -0400

    a few statstics

--- full_cairo.sh
+++ full_cairo.sh
@@ -1,3 +1,2 @@
-#!/bin/sh
-export MAVEN_OPTS="-Xmx7g"
-mvn exec:java -Dexec.mainClass="edu.cmu.cs.lti.cds.runners.FullSystemRunner" -Dexec.args='data//'$1' data event_tuples/'$1' data/duplicate.count.tail'
+export MAVEN_OPTS="-Xmx5g" 
+mvn exec:java -Dexec.mainClass="edu.cmu.cs.lti.cds.runners.FullSystemRunner" -Dexec.args='data/01_discourse_parsed/'$1' data event_tuples/'$1' data/duplicate.count.tail '$1
