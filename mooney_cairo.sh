8d4a04d4355f81e6fff82b4f2a66fbde4ba0ca98
Hector <hunterhector@gmail.com>
2014-11-01 22:18:33 -0400

    cairo side settings

--- mooney_cairo.sh
+++ mooney_cairo.sh
@@ -1,2 +1,2 @@
-export MAVEN_OPTS="-Xmx5g" 
-mvn exec:java -Dexec.mainClass="edu.cmu.cs.lti.cds.runners.FullSystemRunner" -Dexec.args='data/01_discourse_parsed/'$1' data event_tuples/'$1' data/duplicate.count.tail '$1
+export MAVEN_OPTS="-Xmx20g" 
+mvn exec:java -Dexec.mainClass="edu.cmu.cs.lti.cds.runners.script.mooney.MooneyScriptCounterRunner" -Dexec.args='data/02_event_tuples/'$1' data/duplicate.count.tail '$1
