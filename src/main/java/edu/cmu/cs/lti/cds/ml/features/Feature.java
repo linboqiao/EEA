52243770c6ded97d95c33d631381464ca49571fc
hunterhector <hunterhector@gmail.com>
2014-11-30 21:34:04 -0500

    ignore hidden

---
+++ src/main/java/edu/cmu/cs/lti/cds/ml/features/Feature.java
@@ -0,0 +1,10 @@
+package edu.cmu.cs.lti.cds.ml.features;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: zhengzhongliu
+ * Date: 11/30/14
+ * Time: 5:36 PM
+ */
+public class Feature {
+}
---
+++ src/main/java/edu/cmu/cs/lti/cds/ml/features/impl/ArgumentCorefFeature.java
@@ -0,0 +1,10 @@
+package edu.cmu.cs.lti.cds.ml.features.impl;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: zhengzhongliu
+ * Date: 11/30/14
+ * Time: 5:55 PM
+ */
+public class ArgumentCorefFeature {
+}
