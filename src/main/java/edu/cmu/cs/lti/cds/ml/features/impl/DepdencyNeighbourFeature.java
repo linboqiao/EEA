ebdff4d63c40edfdcfbcf2e541dbddfd87608aaa
hunterhector <hunterhector@gmail.com>
2014-12-01 16:19:39 -0500

    remove R

---
+++ src/main/java/edu/cmu/cs/lti/cds/ml/features/impl/DepdencyNeighbourFeature.java
@@ -0,0 +1,10 @@
+package edu.cmu.cs.lti.cds.ml.features.impl;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: zhengzhongliu
+ * Date: 12/1/14
+ * Time: 2:57 PM
+ */
+public class DepdencyNeighbourFeature {
+}
