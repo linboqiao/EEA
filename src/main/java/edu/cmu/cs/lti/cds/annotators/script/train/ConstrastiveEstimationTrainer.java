7038398109c6b0ba5cc0396398ed4018e7b22ad5
hunterhector <hunterhector@gmail.com>
2015-01-07 21:46:07 -0500

    add plot data

---
+++ src/main/java/edu/cmu/cs/lti/cds/annotators/script/train/ConstrastiveEstimationTrainer.java
@@ -0,0 +1,10 @@
+package edu.cmu.cs.lti.cds.annotators.script.train;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: zhengzhongliu
+ * Date: 1/7/15
+ * Time: 3:32 PM
+ */
+public class ConstrastiveEstimationTrainer {
+}
