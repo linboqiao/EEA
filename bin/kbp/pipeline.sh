7abe7ee87b962d6a5d51f83ebb19868ec8a6383e
hunterhector <hunterhector@gmail.com>
2015-08-23 00:39:02 -0400

    Some renames.

--- bin/kbp/pipeline.sh
+++ bin/kbp/pipeline.sh
@@ -1,3 +1,3 @@
 #!/bin/sh 
 export MAVEN_OPTS="-Xmx18g"
-mvn exec:java -pl event-coref -Dexec.mainClass="edu.cmu.cs.lti.event_coref.pipeline.KBP2015EventTaskPipeline" 
+mvn exec:java -pl event-coref -Dexec.mainClass="edu.cmu.cs.lti.event_coref.pipeline.KBP2015EventTaskPipeline"
