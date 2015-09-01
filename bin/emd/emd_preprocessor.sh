4c1f0c4ce58909e0d99d4e0ca2dbb05c22cb850b
hunterhector <hunterhector@gmail.com>
2015-08-31 23:26:26 -0400

    Reorganize runner scripts.

--- bin/emd/emd_preprocessor.sh
+++ bin/emd/emd_preprocessor.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 export MAVEN_OPTS="-Xmx18g"
-mvn exec:java -pl event-mention-detection -Dexec.mainClass="edu.cmu.cs.lti.emd.pipeline.EventMentionDataPreprocessor" -Dexec.args="event-mention-detection/data/Event-mention-detection-2014/LDC2015E03_DEFT_2014_Event_Nugget_Evaluation_Source_Data/data/ event-mention-detection/data/Event-mention-detection-2014/test"
-#mvn exec:java -pl event-mention-detection -Dexec.mainClass="edu.cmu.cs.lti.emd.pipeline.EventMentionDataPreprocessor"
+mvn exec:java -pl event-mention-detection -Dexec.mainClass="edu.cmu.cs.lti.emd.pipeline.twostep.EventMentionDataPreprocessor" -Dexec.args="event-mention-detection/data/Event-mention-detection-2014/LDC2015E03_DEFT_2014_Event_Nugget_Evaluation_Source_Data/data/ event-mention-detection/data/Event-mention-detection-2014/test"
+#mvn exec:java -pl event-mention-detection -Dexec.mainClass="edu.cmu.cs.lti.emd.pipeline.twostep.EventMentionDataPreprocessor"
 #mvn exec:java -pl event-mention-detection -Dexec.mainClass="edu.cmu.cs.lti.emd.pipeline.ACEDataPreprocessor"
--- bin/emd/emd_train.sh
+++ bin/emd/emd_train.sh
@@ -1,2 +1,2 @@
 export MAVEN_OPTS="-Xmx18g" 
-mvn exec:java -pl event-mention-detection -Dexec.mainClass="edu.cmu.cs.lti.emd.pipeline.EventMentionTrainer" -Dexec.args=$1" "$2
+mvn exec:java -pl event-mention-detection -Dexec.mainClass="edu.cmu.cs.lti.emd.pipeline.twostep.EventMentionTrainer" -Dexec.args=$1" "$2
