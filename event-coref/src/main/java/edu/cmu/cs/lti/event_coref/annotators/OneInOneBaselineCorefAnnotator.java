3a1a93fb4b5e0bcdcde663a3a9c5c67e74e8283e
hunterhector <hunterhector@gmail.com>
2015-10-08 22:00:30 -0400

    Update the dependency versions in POM

---
+++ event-coref/src/main/java/edu/cmu/cs/lti/event_coref/annotators/OneInOneBaselineCorefAnnotator.java
@@ -0,0 +1,11 @@
+package edu.cmu.cs.lti.event_coref.annotators;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Date: 10/8/15
+ * Time: 6:02 PM
+ *
+ * @author Zhengzhong Liu
+ */
+public class OneInOneBaselineCorefAnnotator {
+}
