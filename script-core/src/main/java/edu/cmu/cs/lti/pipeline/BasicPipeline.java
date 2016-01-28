96f78952017d3d5ec9b69a68e21e3b392f5d246e
hunterhector <hunterhector@gmail.com>
2016-01-27 19:33:04 -0500

    Adding chinese CoreNLP support.

--- script-core/src/main/java/edu/cmu/cs/lti/pipeline/BasicPipeline.java
+++ script-core/src/main/java/edu/cmu/cs/lti/pipeline/BasicPipeline.java
@@ -1,5 +1,6 @@
 package edu.cmu.cs.lti.pipeline;
 
+import edu.cmu.cs.lti.uima.io.reader.CustomCollectionReaderFactory;
 import edu.cmu.cs.lti.uima.io.writer.CustomAnalysisEngineFactory;
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.uima.UIMAException;
@@ -7,15 +8,22 @@ import org.apache.uima.analysis_engine.AnalysisEngine;
 import org.apache.uima.analysis_engine.AnalysisEngineDescription;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.cas.CAS;
-import org.apache.uima.collection.CollectionException;
-import org.apache.uima.collection.CollectionReader;
+import org.apache.uima.collection.*;
+import org.apache.uima.collection.metadata.CpeDescriptorException;
+import org.apache.uima.fit.cpe.CpeBuilder;
 import org.apache.uima.fit.factory.CollectionReaderFactory;
 import org.apache.uima.fit.pipeline.SimplePipeline;
 import org.apache.uima.resource.ResourceInitializationException;
 import org.apache.uima.util.CasCreationUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.xml.sax.SAXException;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngine;
 import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDescription;
@@ -28,16 +36,90 @@ import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDesc
  * @author Zhengzhong Liu
  */
 public class BasicPipeline {
-    private CollectionReader reader;
-    private AnalysisEngineDescription[] engines;
+    protected final Logger logger = LoggerFactory.getLogger(getClass());
 
+    private CollectionReaderDescription readerDescription;
+    private CollectionReaderDescription outputReader;
+    private AnalysisEngineDescription aggregateAnalysisEngineDesc;
+
+    private CollectionReader cReader;
     private AnalysisEngine aggregateAnalysisEngine;
     private CAS mergedCas;
 
-    public BasicPipeline(ProcessorWrapper wrapper) throws UIMAException {
-        // Create the components
-        reader = CollectionReaderFactory.createReader(wrapper.getCollectionReader());
-        engines = wrapper.getProcessors();
+    private boolean withOutput;
+
+    public BasicPipeline(ProcessorWrapper wrapper) throws UIMAException,
+            CpeDescriptorException, SAXException, IOException {
+        this(wrapper, null, null);
+    }
+
+    public BasicPipeline(ProcessorWrapper wrapper, String workingDir, String outputDir) throws
+            UIMAException, CpeDescriptorException, SAXException, IOException {
+        readerDescription = wrapper.getCollectionReader();
+        AnalysisEngineDescription[] processers = wrapper.getProcessors();
+
+        AnalysisEngineDescription[] engineDescriptions;
+        if (workingDir != null && outputDir != null) {
+            logger.info("Pipeline with output at " + new File(workingDir, outputDir));
+            engineDescriptions = ArrayUtils.add(processers, CustomAnalysisEngineFactory.createXmiWriter(workingDir,
+                    outputDir));
+            outputReader = CustomCollectionReaderFactory.createXmiReader(workingDir, outputDir);
+            withOutput = true;
+        } else {
+            withOutput = false;
+            engineDescriptions = processers;
+        }
+
+        aggregateAnalysisEngineDesc = createEngineDescription(engineDescriptions);
+    }
+
+    private static class StatusCallbackListenerImpl implements StatusCallbackListener {
+
+        private final List<Exception> exceptions = new ArrayList<Exception>();
+
+        private boolean isProcessing = true;
+
+        public void entityProcessComplete(CAS arg0, EntityProcessStatus arg1) {
+            if (arg1.isException()) {
+                for (Exception e : arg1.getExceptions()) {
+                    exceptions.add(e);
+                }
+            }
+        }
+
+        public void aborted() {
+            synchronized (this) {
+                if (isProcessing) {
+                    isProcessing = false;
+                    notify();
+                }
+            }
+        }
+
+        public void batchProcessComplete() {
+            // Do nothing
+        }
+
+        public void collectionProcessComplete() {
+            synchronized (this) {
+                if (isProcessing) {
+                    isProcessing = false;
+                    notify();
+                }
+            }
+        }
+
+        public void initializationComplete() {
+            // Do nothing
+        }
+
+        public void paused() {
+            // Do nothing
+        }
+
+        public void resumed() {
+            // Do nothing
+        }
     }
 
     /**
@@ -47,31 +129,75 @@ public class BasicPipeline {
      * @throws UIMAException
      */
     public void run() throws IOException, UIMAException {
-        SimplePipeline.runPipeline(reader, engines);
+        SimplePipeline.runPipeline(readerDescription, aggregateAnalysisEngineDesc);
     }
 
     /**
-     * Run processor from provided reader, write processed CAS using provided writer.
+     * Run processor from provided reader, write processed CAS as XMI to the given directory.
      *
-     * @param writer THe provided writer
+     * @return A reader description for the processed output.
      * @throws UIMAException
      * @throws IOException
      */
-    public void runWithOutput(AnalysisEngineDescription writer) throws UIMAException, IOException {
-        SimplePipeline.runPipeline(reader, ArrayUtils.add(engines, writer));
+    public CollectionReaderDescription runWithOutput() throws UIMAException, IOException {
+        if (withOutput) {
+            logger.info("Processing with output.");
+            run();
+            return outputReader;
+        } else {
+            throw new IllegalAccessError("Pipeline is not initialized with output.");
+        }
     }
 
     /**
-     * RUn processor from provided reader, write processed CAS as XMI to the given directory.
+     * Run with a CPE from provided reader. CPE use a asynchronous thread pool, which may die during processing.
      *
-     * @param workingDir Parent directory of the data.
-     * @param outputDir  Base directory of the data.
+     * @throws IOException
+     * @throws UIMAException
+     */
+    public void runWithCpe(int numThread) throws IOException, UIMAException, CpeDescriptorException, SAXException {
+        CpeBuilder cpeBuilder = new CpeBuilder();
+        cpeBuilder.setMaxProcessingUnitThreadCount(numThread);
+        cpeBuilder.setReader(readerDescription);
+        cpeBuilder.setAnalysisEngine(aggregateAnalysisEngineDesc);
+
+        final StatusCallbackListenerImpl status = new StatusCallbackListenerImpl();
+
+        CollectionProcessingEngine cpe = cpeBuilder.createCpe(status);
+
+        cpe.process();
+        try {
+            synchronized (status) {
+                while (status.isProcessing) {
+                    status.wait();
+                }
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        if (status.exceptions.size() > 0) {
+            throw new AnalysisEngineProcessException(status.exceptions.get(0));
+        }
+    }
+
+
+    /**
+     * Run processor from provided reader, write processed CAS as XMI to the given directory.
+     *
+     * @return A reader description for the processed output.
      * @throws UIMAException
      * @throws IOException
      */
-    public void runWithOutput(String workingDir, String outputDir) throws UIMAException, IOException {
-        AnalysisEngineDescription writer = CustomAnalysisEngineFactory.createXmiWriter(workingDir, outputDir);
-        runWithOutput(writer);
+    public CollectionReaderDescription runCpeWithOutput(int numThread) throws UIMAException, IOException,
+            CpeDescriptorException, SAXException {
+        if (withOutput) {
+            logger.info("Processing CPE with output.");
+            runWithCpe(numThread);
+            return outputReader;
+        } else {
+            throw new IllegalAccessError("Pipeline is not initialized with output.");
+        }
     }
 
     /**
@@ -80,17 +206,17 @@ public class BasicPipeline {
      * @throws ResourceInitializationException
      */
     public void initialize() throws ResourceInitializationException {
-        // Create AAE
-        final AnalysisEngineDescription aggregateAnalysisEngineDesc = createEngineDescription(engines);
+        // Create Reader
+        cReader = CollectionReaderFactory.createReader(readerDescription);
 
         // Instantiate AAE
         aggregateAnalysisEngine = createEngine(aggregateAnalysisEngineDesc);
 
         // Create CAS from merged metadata
         mergedCas = CasCreationUtils.createCas(
-                Arrays.asList(reader.getMetaData(), aggregateAnalysisEngineDesc.getMetaData())
+                Arrays.asList(cReader.getMetaData(), aggregateAnalysisEngineDesc.getMetaData())
         );
-        reader.typeSystemInit(mergedCas.getTypeSystem());
+        cReader.typeSystemInit(mergedCas.getTypeSystem());
     }
 
     /**
@@ -99,8 +225,8 @@ public class BasicPipeline {
     public void process() {
         try {
             // Process.
-            while (reader.hasNext()) {
-                reader.getNext(mergedCas);
+            while (cReader.hasNext()) {
+                cReader.getNext(mergedCas);
                 aggregateAnalysisEngine.process(mergedCas);
                 mergedCas.reset();
             }
@@ -117,8 +243,8 @@ public class BasicPipeline {
     public void processNext() {
         try {
             // Process.
-            if (reader.hasNext()) {
-                reader.getNext(mergedCas);
+            if (cReader.hasNext()) {
+                cReader.getNext(mergedCas);
                 aggregateAnalysisEngine.process(mergedCas);
                 mergedCas.reset();
             }
