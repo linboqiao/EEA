04cc2cd5cea273d3008705bb9a2d3b9f9ffe2fc6
hunterhector <hunterhector@gmail.com>
2017-07-10 16:59:47 -0400

    Add a bit things to make agiga reader running.

--- script-preprocessor/src/main/java/edu/cmu/cs/lti/script/runners/PreparationPipeline.java
+++ script-preprocessor/src/main/java/edu/cmu/cs/lti/script/runners/PreparationPipeline.java
@@ -1,19 +1,18 @@
-package edu.cmu.cs.lti.script.runners; /**
- *
- */
+package edu.cmu.cs.lti.script.runners;
 
 import edu.cmu.cs.lti.annotators.FanseAnnotator;
 import edu.cmu.cs.lti.collection_reader.AgigaCollectionReader;
+import edu.cmu.cs.lti.pipeline.BasicPipeline;
 import edu.cmu.cs.lti.uima.io.writer.CustomAnalysisEngineFactory;
 import org.apache.uima.UIMAException;
 import org.apache.uima.analysis_engine.AnalysisEngineDescription;
 import org.apache.uima.collection.CollectionReaderDescription;
+import org.apache.uima.collection.metadata.CpeDescriptorException;
 import org.apache.uima.fit.factory.AnalysisEngineFactory;
 import org.apache.uima.fit.factory.CollectionReaderFactory;
-import org.apache.uima.fit.pipeline.SimplePipeline;
-import org.apache.uima.resource.ResourceInitializationException;
 import org.apache.uima.resource.metadata.TypeSystemDescription;
 import org.uimafit.factory.TypeSystemDescriptionFactory;
+import org.xml.sax.SAXException;
 
 import java.io.IOException;
 
@@ -30,42 +29,38 @@ public class PreparationPipeline {
      * @throws org.apache.uima.resource.ResourceInitializationException
      * @throws org.apache.uima.UIMAException
      */
-    public static void main(String[] args) throws IOException, ResourceInitializationException {
+    public static void main(String[] args) throws IOException, UIMAException, SAXException, CpeDescriptorException {
         System.out.println(className + " started...");
 
-        String paramInputDir = args[0];// "/Users/zhengzhongliu/Downloads/agiga_sample"
-        String paramModelBaseDirectory = args[1];// "../models"
+        String inputDir = args[0];// "/Users/zhengzhongliu/Downloads/agiga_sample"
+        String modelBaseDir = args[1];// "../models"
+        String parentOutputDir = args[2];
 
         // Parameters for the writer
-        String paramParentOutputDir = "data";
-        String paramBaseOutputDirName = "fanse_parsed";
-        String paramOutputFileSuffix = null;
+        String baseOutputDirName = "fanse_parsed";
 
         String paramTypeSystemDescriptor = "TypeSystem";
 
-        System.out.println("Reading from " + paramInputDir + " , writing to base dir " + paramBaseOutputDirName);
+        System.out.println("Reading from " + inputDir + " , writing to base dir " + baseOutputDirName);
 
         TypeSystemDescription typeSystemDescription = TypeSystemDescriptionFactory
                 .createTypeSystemDescription(paramTypeSystemDescriptor);
 
         CollectionReaderDescription reader = CollectionReaderFactory.createReaderDescription(
                 AgigaCollectionReader.class, typeSystemDescription,
-                AgigaCollectionReader.PARAM_INPUTDIR, paramInputDir);
+                AgigaCollectionReader.PARAM_DATA_PATH, inputDir,
+                AgigaCollectionReader.PARAM_FILE_EXTENSION, ".xml.gz"
+        );
 
         AnalysisEngineDescription fanseParser = AnalysisEngineFactory.createEngineDescription(
                 FanseAnnotator.class, typeSystemDescription, FanseAnnotator.PARAM_MODEL_BASE_DIR,
-                paramModelBaseDirectory);
+                modelBaseDir);
 
         AnalysisEngineDescription writer = CustomAnalysisEngineFactory.createGzippedXmiWriter(
-                paramParentOutputDir, paramBaseOutputDirName, 0, paramOutputFileSuffix);
+                parentOutputDir, baseOutputDirName, 0, null);
 
         // Run the pipeline.
-
-        try {
-            SimplePipeline.runPipeline(reader, fanseParser, writer);
-        } catch (UIMAException e) {
-            e.printStackTrace();
-        }
+        new BasicPipeline(reader, 4, fanseParser, writer).run();
 
         System.out.println(className + " successfully completed.");
     }
