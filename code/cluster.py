a181234ee9d6a982cfafc28e82ec6d8c5e728c33
franticnerd <chao.uiuc@gmail.com>
2017-09-19 16:55:29 -0500

    clustering added

---
+++ code/cluster.py
@@ -0,0 +1,50 @@
+from collections import defaultdict
+
+from scipy.spatial.distance import cosine
+from spherecluster import SphericalKMeans
+from dataset import SubDataSet
+
+class Clusterer:
+
+    def __init__(self, data, n_cluster):
+        self.data = data
+        self.n_cluster = n_cluster
+        self.clus = SphericalKMeans(n_cluster)
+        self.clusters = defaultdict(list)  # cluster id -> members
+        self.membership = None  # a list contain the membership of the data points
+        self.center_ids = None  # a list contain the ids of the cluster centers
+        self.inertia_scores = None
+
+    def fit(self):
+        self.clus.fit(self.data)
+        labels = self.clus.labels_
+        for idx, label in enumerate(labels):
+            self.clusters[label].append(idx)
+        self.membership = labels
+        self.center_ids = self.gen_center_idx()
+        self.inertia_scores = self.clus.inertia_
+        print 'Clustering concentration score:', self.inertia_scores
+
+    # find the idx of each cluster center
+    def gen_center_idx(self):
+        ret = []
+        for cluster_id in xrange(self.n_cluster):
+            center_idx = self.find_center_idx_for_one_cluster(cluster_id)
+            ret.append((cluster_id, center_idx))
+        return ret
+
+    def find_center_idx_for_one_cluster(self, cluster_id):
+        query_vec = self.clus.cluster_centers_[cluster_id]
+        members = self.clusters[cluster_id]
+        best_similarity, ret = -1, -1
+        for member_idx in members:
+            member_vec = self.data[member_idx]
+            cosine_sim = self.calc_cosine(query_vec, member_vec)
+            if cosine_sim > best_similarity:
+                best_similarity = cosine_sim
+                ret = member_idx
+        return ret
+
+    def calc_cosine(self, vec_a, vec_b):
+        return 1 - cosine(vec_a, vec_b)
+
