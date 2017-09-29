10b2faa775d1d7c66de419ac179b75008c32179b
hunterhector <hunterhector@gmail.com>
2017-09-29 16:20:30 -0400

    Add a support sentence writer; Add a cogcomp-nlp semantic parser runner;

--- salience/src/main/java/edu/cmu/cs/lti/salience/stats/NytEntityCounter.java
+++ salience/src/main/java/edu/cmu/cs/lti/salience/stats/NytEntityCounter.java
@@ -1,5 +1,9 @@
 package edu.cmu.cs.lti.salience.stats;
 
+import edu.cmu.cs.lti.uima.annotator.AbstractLoggingAnnotator;
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.jcas.JCas;
+
 /**
  * Created with IntelliJ IDEA.
  * Date: 9/7/17
@@ -7,6 +11,10 @@ package edu.cmu.cs.lti.salience.stats;
  *
  * @author Zhengzhong Liu
  */
-public class NytEntityCounter {
-    
+public class NytEntityCounter extends AbstractLoggingAnnotator{
+
+    @Override
+    public void process(JCas aJCas) throws AnalysisEngineProcessException {
+
+    }
 }
