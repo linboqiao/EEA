4d73f5ce0bd2f544be813e9b291a659a7ce50c2d
hunterhector <hunterhector@gmail.com>
2018-02-08 04:46:14 -0500

    add text reader.

--- event-coref/src/main/java/edu/cmu/cs/lti/event_coref/pipeline/RunOnlyPipeline.java
+++ event-coref/src/main/java/edu/cmu/cs/lti/event_coref/pipeline/RunOnlyPipeline.java
@@ -1,9 +1,11 @@
 package edu.cmu.cs.lti.event_coref.pipeline;
 
 import edu.cmu.cs.lti.collection_reader.LDCXmlCollectionReader;
+import edu.cmu.cs.lti.uima.io.reader.PlainTextCollectionReader;
 import edu.cmu.cs.lti.utils.Configuration;
 import org.apache.uima.collection.CollectionReaderDescription;
 import org.apache.uima.fit.factory.CollectionReaderFactory;
+import org.apache.uima.resource.ResourceInitializationException;
 import org.apache.uima.resource.metadata.TypeSystemDescription;
 import org.uimafit.factory.TypeSystemDescriptionFactory;
 
@@ -35,7 +37,21 @@ public class RunOnlyPipeline {
         String outputPath = argv[2];
         //"../data/project_data/cmu-script/mention/kbp/chinese/Chinese_Coled_Start_LDC2016E63"
 
-        CollectionReaderDescription reader = CollectionReaderFactory.createReaderDescription(
+//        CollectionReaderDescription reader = ldcReader(typeSystemDescription, inputPath, kbpConfig);
+        CollectionReaderDescription reader = textReader(typeSystemDescription, inputPath, kbpConfig);
+
+        // Now prepare the real pipeline.
+        EventMentionPipeline pipeline = new EventMentionPipeline(typeSystemName, kbpConfig);
+
+        boolean skipTestPrepare = kbpConfig.getBoolean("edu.cmu.cs.lti.test.skip.preprocess", false);
+        pipeline.prepareData(kbpConfig, outputPath, skipTestPrepare, reader);
+
+        pipeline.runVanilla(kbpConfig, outputPath);
+    }
+
+    private static CollectionReaderDescription ldcReader(TypeSystemDescription typeSystemDescription, String inputPath,
+                                                         Configuration kbpConfig) throws ResourceInitializationException {
+        return CollectionReaderFactory.createReaderDescription(
                 LDCXmlCollectionReader.class, typeSystemDescription,
                 LDCXmlCollectionReader.PARAM_DATA_PATH, inputPath,
                 LDCXmlCollectionReader.PARAM_BASE_NAME_FILE_FILTER,
@@ -46,13 +62,15 @@ public class RunOnlyPipeline {
                 kbpConfig.get("edu.cmu.cs.lti.language"),
                 LDCXmlCollectionReader.PARAM_RECURSIVE, true
         );
+    }
 
-        // Now prepare the real pipeline.
-        EventMentionPipeline pipeline = new EventMentionPipeline(typeSystemName, kbpConfig);
-
-        boolean skipTestPrepare = kbpConfig.getBoolean("edu.cmu.cs.lti.test.skip.preprocess", false);
-        pipeline.prepareData(kbpConfig, outputPath, skipTestPrepare, reader);
-
-        pipeline.runVanilla(kbpConfig, outputPath);
+    private static CollectionReaderDescription textReader(TypeSystemDescription typeSystemDescription, String inputPath,
+                                                          Configuration kbpConfig) throws ResourceInitializationException {
+        return    CollectionReaderFactory.createReaderDescription(
+                PlainTextCollectionReader.class, typeSystemDescription,
+                PlainTextCollectionReader.PARAM_INPUTDIR, inputPath,
+                PlainTextCollectionReader.PARAM_LANGUAGE, kbpConfig.get("edu.cmu.cs.lti.language"),
+                PlainTextCollectionReader.PARAM_DO_NOISE_FILTER, true
+        );
     }
 }
