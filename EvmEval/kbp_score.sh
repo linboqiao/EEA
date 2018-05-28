4b7f937a83fd01b28fcd6fc1485361f2d6f8962f
linbo <linboqiao@gmail.com>
2018-05-28 16:24:26 +0800

    EvmEval update

--- EvmEval/kbp_score.sh
+++ EvmEval/kbp_score.sh
@@ -4,7 +4,7 @@ scorer=./bin/scorer_v1.8.py
 gold_dir=/home/linbo/workspace/Datasets/LDCData/TAC/LDC2016E36_TAC_KBP_English_Event_Nugget_Detection_and_Coreference_Comprehensive_Training_and_Evaluation_Data_2014-2015/conversion
 
 echo "Evaluating system A: cmu-script\n Stored score report at output/score.txt\n"
-$scorer -g $gold_dir/gold_nugget -s /home/linbo/workspace/Xing/TA/cmu-script/sample-output/eval/full_run/lv1_coref.tbf -t $gold_dir/tkn/ -d ./output/A_out.tmp -o ./output/score_cmu-script.txt
+$scorer -g $gold_dir/gold_nugget -s /home/linbo/workspace/GitHubs/TA/cmu-script/sample-output/eval/full_run/lv1_coref.tbf -t $gold_dir/tkn/ -d ./output/A_out.tmp -o ./output/score_cmu-script.txt
 more output/score_cmu-script.txt
 
 
